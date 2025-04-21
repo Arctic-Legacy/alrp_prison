c0ed9fb74f46d59b58a6fd564568b59d1e3130cc
NotSomething0 <54156973+NotSomething0@users.noreply.github.com>
2025-04-20 22:45:57 -0500

    feat(types): create type declerations for class constructors

---
+++ shared/types.lua
@@ -0,0 +1,15 @@
+---@diagnostic disable: missing-return
+---@alias VectorTable { x: number|string, y: number|string, z:number|string }
+
+---Create a new instance of CProcessingPoint
+---@param name string
+---@param coordinate vector3
+---@param distance number
+---@return CProcessingPoint
+function CProcessingPoint:new(name, coordinate, distance)
+end
+
+---Create a new instance of CConfigStore
+---@return CConfigStore
+function CConfigStore:new()
+end
